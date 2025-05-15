f8771fe0da2f3f97a8551c529e7df99638177cd9
Letícia Brondi <leebrondi@gmail.com>
2025-05-15 14:40:51 -0300

    atualizando modo claro e escuro nas páginas, e correções no js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const toggleBtn = document.getElementById('toggleTheme');
 const body = document.body;
 
+body.classList.add('light');
+toggleBtn.textContent = '🌙';
+
 toggleBtn.addEventListener('click', () => {
-    body.classList.toggle('light');
-    toggleBtn.textContent = body.classList.contains('light') ? '☀️' : '🌙';
+  const isLight = body.classList.contains('light');
+
+  if (isLight) {
+    body.classList.remove('light');
+    body.classList.add('dark');
+    toggleBtn.textContent = '☀️';
+  } else {
+    body.classList.remove('dark');
+    body.classList.add('light');
+    toggleBtn.textContent = '🌙';
+  }
 });
-
-
